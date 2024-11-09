a8310412178d42015864d3960dcf82d69b19e88d
Rodrigo García <yo@rodrigogarcia.me>
2024-11-09 12:00:21 -0600

    Add PrimeVue

---
+++ src/views/LoginView.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import Button from 'primevue/button'
+</script>
+
+<template>
+  <div class="about">
+    <h1>Login</h1>
+
+    <Button>Login</Button>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
